89baa4c1c3ce1b9a9a05852f3cf0964b0e250ade
Jhonata Ewerton <jhonata.as@hotmail.com>
2024-08-24 17:26:33 -0300

    finish login screen

--- desafio-fe-bold/src/app/modules/login/login.component.ts
+++ desafio-fe-bold/src/app/modules/login/login.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { environment } from 'src/app/environment/environment';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  readonly version = environment.version;
 
 }
--- desafio-fe-bold/src/app/shared/shared.module.ts
+++ desafio-fe-bold/src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './components/input/input.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ButtonComponent, InputComponent],
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, MatInputModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, MatInputModule, HttpClientModule],
   exports: [ButtonComponent, InputComponent],
 })
 export class SharedModule {}
