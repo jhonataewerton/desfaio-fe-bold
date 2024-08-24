40425252acf6fd258ce37f9a75cfb0dcb552e8f5
Jhonata Ewerton <jhonata.as@hotmail.com>
2024-08-24 14:14:02 -0300

    create button component and added Angular Material

--- desafio-fe-bold/src/app/app.module.ts
+++ desafio-fe-bold/src/app/app.module.ts
@@ -5,19 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module';
 import { LoginModule } from './modules/login/login.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HomeModule,
-    LoginModule
+    LoginModule,
+    SharedModule
   ],
-   providers: [],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- desafio-fe-bold/src/app/modules/login/login.module.ts
+++ desafio-fe-bold/src/app/modules/login/login.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LOGIN_ROUTES } from './login.routing';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ButtonComponent } from 'src/app/shared/button/button.component';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(LOGIN_ROUTES)
+    RouterModule.forChild(LOGIN_ROUTES),
+    SharedModule
   ],
   exports: [LoginComponent],
 })
