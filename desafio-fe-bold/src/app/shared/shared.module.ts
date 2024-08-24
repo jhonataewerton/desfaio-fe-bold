4762d56afdf10752e4e8fd263c2f0527a23ad4f8
Jhonata Ewerton <jhonata.as@hotmail.com>
2024-08-24 14:53:12 -0300

    create input component and added logo

--- desafio-fe-bold/src/app/shared/shared.module.ts
+++ desafio-fe-bold/src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 import { ButtonComponent } from './components/button/button.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { InputComponent } from './components/input/input.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [ButtonComponent],
-  imports: [CommonModule],
-  exports: [ButtonComponent],
+  declarations: [ButtonComponent, InputComponent],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, MatInputModule],
+  exports: [ButtonComponent, InputComponent],
 })
 export class SharedModule {}
